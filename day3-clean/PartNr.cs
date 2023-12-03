bb59844ae7f2b53231b6c33893ca46dd26344390
Sebastiaan Megens <sebastiaan.megens@greenchoice.nl>
2023-12-03 07:52:15 +0100

    Added: comment.

--- day3-clean/PartNr.cs
+++ day3-clean/PartNr.cs
@@ -1,4 +1,9 @@
-
+/// <summary>
+/// PartNr here has not completely the same meaning as in the puzzle.
+/// This class is more a _possible_ part number. The Schematic class
+/// has the context to determine if a possible part number is an
+/// actual part number.
+/// </summary>
 class PartNr
 {
     public Point Position { get; private set; }
