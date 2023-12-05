f0405e3b6354027acaaa4ba909ca0bfdd4ed5044
Sebastiaan <sebastiaan@blauwe-lucht.nl>
2023-12-05 16:52:14 +0100

    Added: script to generate code for new day.

---
+++ helpers/new-day.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -euo pipefail
+
+show_help() {
+    echo "Usage: $0 DAY"
+    echo "This script creates two directories, dayDAY-clean and dayDAY-fast, and sets up .NET solutions and projects in them."
+    echo "DAY should be the day number for which you want to set up the directories and projects."
+}
+
+if [ "$#" -ne 1 ]; then
+    show_help
+    exit 1
+fi
+
+DAY=$1
+
+echo "Creating and setting up day$DAY-clean..."
+mkdir "day$DAY-clean"
+cd "day$DAY-clean"
+dotnet new sln -n "day$DAY"
+dotnet new console -n Solver
+dotnet sln "day$DAY.sln" add Solver/Solver.csproj
+dotnet new nunit -n Solver.Tests
+dotnet sln "day$DAY.sln" add Solver.Tests/Solver.Tests.csproj
+cd ..
+
+echo "Creating and setting up day$DAY-fast..."
+mkdir "day$DAY-fast"
+cd "day$DAY-fast"
+dotnet new sln -n "day$DAY"
+dotnet new console -n Solver
+dotnet sln "day$DAY.sln" add Solver/Solver.csproj
+dotnet new nunit -n Solver.Tests
+dotnet sln "day$DAY.sln" add Solver.Tests/Solver.Tests.csproj
+cd ..
+
