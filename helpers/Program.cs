e67897dc1d1c7c9ea39fc46f93938e14ade46872
Sebastiaan <sebastiaan@blauwe-lucht.nl>
2023-12-05 09:10:24 +0100

    Added: helper code to speed up building solutions.

---
+++ helpers/Program.cs
@@ -0,0 +1,23 @@
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] lines = File.ReadAllLines("input.txt");
+        SolvePart1(lines);
+        SolvePart2(lines);
+    }
+
+    private static void SolvePart1(string[] lines)
+    {
+        int total = 0;
+        foreach (string line in lines)
+        {
+
+        }
+        Console.WriteLine($"Part 1: {total}");
+    }
+
+    private static void SolvePart2(string[] lines)
+    {
+    }
+}
